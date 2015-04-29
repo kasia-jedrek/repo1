6f6ab15ab6a642dec1cd873b3f1165720c4610ad
yeetyupb <katarzynajedrzejewska@gmail.com>
2015-04-29 23:26:46 +0200

    fewtretetertertre

--- sliwka/src/Main.java
+++ sliwka/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Kasia ma psa ");
-System.out.println("Kasia  jeeeed ");
+System.out.println("Kasia  jeeeed hthytjytjyj");
 	}
 
 }
