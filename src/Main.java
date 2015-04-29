e9c0823c0ea4a127c19656eafcbbdd3acacd3b15
Kasia Jee <kasia.jedrek@gmail.com>
2015-04-29 23:33:49 +0200

    AAAAAA

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,13 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Kasia ma psa ");
+System.out.println("Kasia ma psa ");
+System.out.println("Kasia ma psa2 ");
+
+System.out.println("AAAAAA");
+System.out.println("Kasia ma psa2 ");
+System.out.println("Kasia ma psa ");
+System.out.println("Kasia ma psa2 ");
 System.out.println("Kasia ma psa2 ");
 	}
 
