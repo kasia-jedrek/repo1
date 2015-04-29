b022fb9d93ccbd6c197db646e0e4f1da15d35c70
Kasia Jee <kasia.jedrek@gmail.com>
2015-04-29 23:54:50 +0200

    wwwwwwwwwwwwwwwwwwwwww

--- sliwka/src/Main.java
+++ sliwka/src/Main.java
@@ -5,6 +5,17 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("SIA LA LA");
+
+System.out.println("Kasia ma psa ");
+System.out.println("Kasia ma psa ");
+System.out.println("Kasia  jeeeed hthytjytjyj");
+System.out.println("Kasia ma psa ");
+System.out.println("Kasia  jeeeed hthytjytjyj");
+
+System.out.println("Kasia ma psa ");
+System.out.println("Kasia  jeeeed hthytjytjyj");
+System.out.println("Kasia  jeeeed hthytjytjyj");
+	}
 	}
 
 }
